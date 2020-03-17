e87424ee3741e48e9e03f7ead5983b0732f9ba75
aposipa <andrew.posipanka@gmail.com>
2020-03-17 17:59:02 -0400

    Finished MVP goals with some minor tweaks left to do.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { carReducer } from './components/Reducers/carReducer';
+import { createStore } from 'redux';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(carReducer);
+console.log("*******", store.getState());
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}>
+    <App />
+    </Provider>, 
+    rootElement);
